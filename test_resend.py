9b9e0f1e865c8d0c4220ee3a4f9f3df6a61db3d6
sathwikreddyshamakuri <shyamakurisathwik@gmail.com>
2025-08-19 17:47:03 -0400

    chore: structure API under app/; add main.py & emailer.py; move routers/

---
+++ test_resend.py
@@ -0,0 +1,10 @@
+﻿import os, resend
+resend.api_key = os.environ['RESEND_API_KEY']
+r = resend.Emails.send({
+    'from': 'Cloud Photo Share <no-reply@send.naugevault.com>',
+    'to': ['youraddress@example.com'],
+    'subject': 'Test from Resend',
+    'html': '<p>It works!</p>',
+    'reply_to': 'support@naugevault.com',
+})
+print('Sent:', getattr(r, 'id', r))
